74978e999288745c77d6b7e5379c82e6a1fd126c
Yujing <gyj9907@gmail.com>
2019-10-29 08:38:40 +0100

    test

--- app/src/main/java/com/example/sep4android/PlantListActivity.java
+++ app/src/main/java/com/example/sep4android/PlantListActivity.java
@@ -1,6 +1,13 @@
 package com.example.sep4android;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,18 +15,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class PlantListActivity extends AppCompatActivity {
+public class PlantListActivity extends AppCompatActivity /*implements PlantAdapter.onPlantListener*/ {
 
     RecyclerView mPlantList;
     RecyclerView.Adapter mPlantAdapter;
+    ArrayList<Plant> plants = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plantlist);
 
-        ArrayList<Plant> plants = new ArrayList<>();
 
+        //test for list
         plants.add(new Plant("rose","roses",15,30,12,22));
         plants.add(new Plant("jasmine","jasmines",16,30,22,22));
 
@@ -32,7 +40,15 @@ public class PlantListActivity extends AppCompatActivity {
         mPlantList.setLayoutManager(new LinearLayoutManager(this));
         mPlantList.setAdapter(mPlantAdapter);
 
+
+
     }
 
-
+    //list of items should be individually a button to jump to its profile >> missing
+   /* @Override
+    public void onPlantClick(int position) {
+        plants.get(position);
+        Intent intent = new Intent(this, PlantInfoActivity.java);
+        startActivity(intent);
+    }*/
 }
