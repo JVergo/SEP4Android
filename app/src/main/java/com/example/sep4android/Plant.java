5b1142bde675a61b46162c9b48990b73eda5e4ac
jvergo <jvergoa@gmail.com>
2019-10-31 13:00:00 +0100

    Work on Plnat data container and plant profiles

--- app/src/main/java/com/example/sep4android/Plant.java
+++ app/src/main/java/com/example/sep4android/Plant.java
@@ -1,4 +1,48 @@
 package com.example.sep4android;
 
 public class Plant {
+    private String plantName;
+    private PlantProfile profile;
+    private int co2, temp, humidity, light;
+
+    public Plant(String plantName, PlantProfile profile)
+    {
+        this.plantName = plantName;
+        this.profile = profile;
+    }
+
+    public String getPlantName() {
+        return plantName;
+    }
+    public PlantProfile getProfile() {
+        return profile;
+    }
+
+    public int getCo2() {
+        return co2;
+    }
+    public void setCo2(int co2) {
+        this.co2 = co2;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    public int getHumdity() {
+        return humidity;
+    }
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public int getLight() {
+        return light;
+    }
+    public void setLight(int light) {
+        this.light = light;
+    }
 }
