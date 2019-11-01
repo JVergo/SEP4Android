f86853a5bde7b5242bf513f56bd8a12582110a8f
Yujing <gyj9907@gmail.com>
2019-11-01 10:10:05 +0100

    plant profile list item and plant info without any functions created yet

---
+++ app/src/main/java/com/example/sep4android/PlantProfile.java
@@ -0,0 +1,15 @@
+package com.example.sep4android;
+
+public class PlantProfile {
+
+    private String plantProfileName;
+
+    public PlantProfile(String plantProfileName) {
+        this.plantProfileName = plantProfileName;
+    }
+
+
+    public String getPlantProfileName() {
+        return plantProfileName;
+    }
+}
